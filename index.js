231632ad244b73a2bb0d08ae8a43fd1402d93581
Jose Neto <josevieira2029@gmail.com>
2023-10-05 14:54:32 -0300

    Definindo tabela ocorrencia

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const { Sequelize, DataTypes } = require('sequelize');
+const { v4: uuidv4 } = require('uuid');
 
-const tabela = new Sequelize('atividade', 'postgres', 'postgres', {
+
+const BancoDeDados = new Sequelize('projeto', 'postgres', 'postgres', {
   host: 'localhost',
   dialect: 'postgres'
 });
 
 async function conectar() {
   try {
-    await tabela.authenticate();
+    await BancoDeDados.authenticate();
     console.log('ok');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
@@ -16,15 +18,28 @@ async function conectar() {
 
 conectar();
 
-const Usuario = tabela.define('Usuario', {
+const Ocorrencia = BancoDeDados.define('Ocorrencia', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: Sequelize.UUIDV4,
+    primaryKey: true,
+  },
   nome: {
     type: DataTypes.STRING,
     allowNull: false,
   },
-  email: {
+  tipo: {
     type: DataTypes.STRING,
     allowNull: false,
   },
+  datHora: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  geometria: {
+    type: DataTypes.GEOMETRY,
+    
+  }
 });
 
 
