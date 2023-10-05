9cf23996ee1e7eebd8ee189ee5598558947db304
joyce <joyce069.js@gmail.com>
2023-10-05 19:46:19 -0300

    criando um html para teste

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const { v4: uuidv4 } = require('uuid');
+const express= require('express');
+
+const app = express();
+app.use(express.json());
 
 
-const BancoDeDados = new Sequelize('projeto', 'postgres', 'postgres', {
+//..................... Criando o banco  ...................
+const bancoDeDados = new Sequelize('projeto', 'postgres', 'postgres', {
   host: 'localhost',
   dialect: 'postgres'
 });
 
 async function conectar() {
   try {
-    await BancoDeDados.authenticate();
+    await bancoDeDados.authenticate();
     console.log('ok');
   } catch (error) {
     console.error('A conexão fallhou :(', error);
-  }
+  } 
 }
 
 conectar();
 
-const Ocorrencia = BancoDeDados.define('Ocorrencia', {
+//..................... Definindo a estrutura da tabela Ocorrencia ...................
+const Ocorrencia = bancoDeDados.define('Ocorrencia', {
   id: {
     type: DataTypes.UUID,
-    defaultValue: Sequelize.UUIDV4,
+    defaultValue: DataTypes.UUIDV4,
     primaryKey: true,
   },
   nome: {
@@ -34,7 +40,7 @@ const Ocorrencia = BancoDeDados.define('Ocorrencia', {
   },
   datHora: {
     type: DataTypes.DATE,
-    allowNull: false,
+    allowNull: false
   },
   geometria: {
     type: DataTypes.GEOMETRY,
@@ -44,8 +50,38 @@ const Ocorrencia = BancoDeDados.define('Ocorrencia', {
 
 
 async function sincronizar() {
-  await Ponto.sync();
+  await Ocorrencia.sync();
   console.log("Sincronizando");
 }
 
 sincronizar();
+
+const salvando = () => {
+  const titulo = document.getElementById(campoDeTexto).value
+  const dados = {
+    nome: titulo,
+    tipo: "gddh",
+    datHora: "jshgd"
+  }
+
+  fetch('http://localhost:3333/ocorrencia', {
+    method: 'POST',
+    body: JSON.stringify(dados)
+  })
+}
+
+// //..................... Criando as rotas ...................
+
+app.post("/ocorrencia", async (req,res) => {
+  const novaOcorrencia = req.body.nome
+ // res.send("");
+  console.log(novaOcorrencia);
+});
+
+
+//salvando();
+
+app.listen(3333, () => {
+  console.log("Funcionando");
+}
+);
