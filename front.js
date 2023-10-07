476ab8dfa720335df43f4a39245d1ae9d79aa643
joyce <joyce069.js@gmail.com>
2023-10-06 23:55:18 -0300

    criando o css

--- front.js
+++ front.js
@@ -41,3 +41,48 @@ function listar() {
         });
 };
 
+
+
+
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: -23.550520, lng: -46.633308 },
+        zoom: 14
+    });
+
+    var geocoder = new google.maps.Geocoder();
+
+    google.maps.event.addListener(map, 'click', function(event) {
+        var marker = new google.maps.Marker({
+            position: event.latLng,
+            map: map
+        });
+
+        geocoder.geocode({ 'location': event.latLng }, function(results, status) {
+            if (status === 'OK') {
+                if (results[0]) {
+                    console.log('Endereço:', results[0].formatted_address);
+                }
+            }
+        });
+    });
+
+    var form = document.getElementById('address-form');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        var address = document.getElementById('address').value;
+
+        geocoder.geocode({ 'address': address }, function(results, status) {
+            if (status === 'OK') {
+                var location = results[0].geometry.location;
+                var marker = new google.maps.Marker({
+                    position: location,
+                    map: map
+                });
+                console.log('Novo Marcador Adicionado:', location.lat(), location.lng());
+            } else {
+                console.error('Não foi possível encontrar o endereço:', status);
+            }
+        });
+    });
+}
